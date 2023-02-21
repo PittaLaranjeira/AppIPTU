a8aeb44f2549e1f789ea221afd35ef32151abdff
pittalaranjeira <vitoria.pitta01@fatec.sp.gov.br>
2023-02-21 16:52:35 -0300

    inserção cálculo IPTU

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,24 @@ export class HomePage {
 
   constructor() {}
 
+  resultado = 0;
+  iptu = 0;
+
+  calciptu(formu: { value: { ida: any; valimo: any; }; }) {
+    const idade = formu.value.ida;
+    const vimovel = formu.value.valimo;
+
+    if (idade <= 39) {
+      this.iptu = 20;
+    } else if (40 <= idade && idade <= 59) {
+      this.iptu = 10;
+    } else if (60 <= idade && idade <= 79) {
+      this.iptu = 5;
+    } else if (80 <= idade) {
+      this.iptu = 0;
+    }
+
+    this.resultado = (this.iptu * parseFloat(vimovel)) / 100;
+  }
+
 }
